6581e2b78f16d753389f3db4396542c5231e6375
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-02-03 00:14:57 +0500

    Run init_db at startup in production and further fix AdminDashboard chart

---
+++ ssh_cat_init_db.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/expect -f
+set timeout 30
+set host "root@94.183.184.106"
+set password "Code2025#"
+
+spawn ssh $host "cat /var/www/dacha/backend/app/db/init_db.py"
+expect {
+    "password:" {
+        send "$password\r"
+    }
+}
+expect eof
