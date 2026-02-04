c420666a585d75a23cc57aa7fde3bafddb768112
Temur Yo`ldoshev <97717715+Tamurbek@users.noreply.github.com>
2026-02-04 15:30:27 +0500

    remove temporary scripts

--- ssh_check_v2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect -f
-set timeout 60
-spawn ssh root@94.183.184.106 "docker compose version && git --version"
-expect {
-    "password:" {
-        send "Code2025#\r"
-    }
-}
-expect eof
